e52ad836946ba62fe93afb19228bcdaddbf34d54
LKH <kangholee8512@gmail.com>
2023-06-11 17:06:54 +0900

    방 상세화면  UI추가

---
+++ app/src/main/java/com/cnc/landapp_kh/ViewRoomDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.cnc.landapp_kh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewRoomDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_room_detail)
+    }
+}
